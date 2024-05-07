e892049f3a17d0f787f942da93c8ee46a03b0443
Gurman Singh <gurmandhanoa01@gmail.com>
2024-05-07 15:46:26 +0530

    cors policy updated

--- src/app.js
+++ src/app.js
@@ -5,10 +5,23 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
+const allowedOrigins = process.env.ALLOWED_ORIGINS;
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },credentials: true,
+}));
+
+app.get("/", function (req, res) {
+    res.status(200).json({
+        success: true,
+        message: "Backend is working successfully!!!"
+      });
+});
 
 app.use(express.json({limit: "5mb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
